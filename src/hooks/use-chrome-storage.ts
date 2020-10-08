9d829a76a8777f905d2293e620a9b89c23e6e7ee
Narin Luangrath <narin.luangrath@gmail.com>
2020-10-08 15:27:15 -0700

    chore: rename vars

--- src/hooks/use-chrome-storage.ts
+++ src/hooks/use-chrome-storage.ts
@@ -1,43 +1,48 @@
 import React from "react";
 
-interface GetStorageProps<T> {
+interface GetStorage<T> {
   fetching: boolean;
   data: T | null;
+  refetch: () => void;
 }
 
 export const useGetStorage = <T>(
   key: string,
   chr?: typeof chrome
-): GetStorageProps<T> => {
+): GetStorage<T> => {
   const [fetching, setFetching] = React.useState(true);
   const [data, setData] = React.useState<T>(null);
 
-  React.useEffect(() => {
+  const refetch = React.useCallback(() => {
     chr = chr || chrome;
     chr.storage.sync.get([key], (result) => {
       setData(result[key]);
       setFetching(false);
     });
   }, [key, chr]);
+  React.useEffect(refetch, [refetch]);
 
-  return { fetching, data };
+  return { fetching, data, refetch };
 };
 
-interface SetStorageProps {
+interface SetStorage {
   fetching: boolean;
   setStorage: (key: string, value: any) => void;
 }
 
-export const useSetStorage = (chr?: typeof chrome): SetStorageProps => {
+export const useSetStorage = (chr?: typeof chrome): SetStorage => {
   const [fetching, setFetching] = React.useState(false);
 
-  const setStorage = React.useCallback((key, value) => {
-    chr = chr || chrome;
-    setFetching(true);
-    chr.storage.sync.set({ [key]: value }, () => {
-      setFetching(false);
-    });
-  }, []);
+  const setStorage = React.useCallback(
+    (key, value) => {
+      chr = chr || chrome;
+      setFetching(true);
+      chr.storage.sync.set({ [key]: value }, () => {
+        setFetching(false);
+      });
+    },
+    [chr]
+  );
 
   return { fetching, setStorage };
 };
